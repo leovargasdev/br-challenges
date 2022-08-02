83f17583e25a71da2c75fde48baa7c351e24ae77
Leonardo Vargas <leu1607@hotmail.com>
2022-08-01 22:00:21 -0300

    feat: criando redirect na página home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+
 import 'styles/globals.scss'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,58 @@
 import Image from 'next/image'
-import { useSession, signOut } from 'next-auth/react'
+import { signOut, getSession } from 'next-auth/react'
 
 import styles from 'styles/home.module.scss'
+import { GetServerSideProps, NextPage } from 'next'
 
-const HomePage = () => {
-  const { data } = useSession()
+interface UserProps {
+  name: string
+  email: string
+  image?: string
+}
 
-  return (
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <div className={styles.user}>
-          {data?.user?.image && (
-            <div className={styles.user__avatar}>
-              <Image src={data?.user?.image} layout="fill" objectFit="cover" />
-            </div>
-          )}
-          <div className={styles.user__info}>
-            <strong>{data?.user?.name}</strong>
-            <span>{data?.user?.email}</span>
+const HomePage: NextPage<UserProps> = user => (
+  <div className={styles.container}>
+    <header className={styles.header}>
+      <div className={styles.user}>
+        {user?.image && (
+          <div className={styles.user__avatar}>
+            <Image src={user?.image} layout="fill" objectFit="cover" />
           </div>
+        )}
+        <div className={styles.user__info}>
+          <strong>{user.name}</strong>
+          <span>{user.email}</span>
         </div>
+      </div>
 
-        <button
-          type="button"
-          onClick={() => signOut()}
-          className={styles.signOut}
-        >
-          SAIR
-        </button>
-      </header>
+      <button
+        type="button"
+        onClick={() => signOut()}
+        className={styles.signOut}
+      >
+        SAIR
+      </button>
+    </header>
 
-      <h1>tela inicial</h1>
-    </div>
-  )
+    <h1>tela inicial</h1>
+  </div>
+)
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req })
+
+  if (session === null) {
+    return {
+      props: {},
+      redirect: {
+        destination: '/login'
+      }
+    }
+  }
+
+  const user = session?.user || {}
+
+  return { props: user }
 }
 
 export default HomePage
