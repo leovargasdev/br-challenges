a822ad7ec762c31ca84e7ca53adf0fb22f2422a8
Leonardo Vargas <leu1607@hotmail.com>
2022-08-04 21:20:54 -0300

    build: adicionando envs do prismic

--- src/service/prismic.ts
+++ src/service/prismic.ts
@@ -1,18 +1,17 @@
 import * as prismicNext from '@prismicio/next'
 import { createClient, getRepositoryName } from '@prismicio/client'
 
-const prismicUrl = 'https://app-desafios.prismic.io/api/v2'
+const prismicURL = process.env.PRISMIC_URL || ''
 
-export const repositoryName = getRepositoryName(prismicUrl)
+export const repositoryName = getRepositoryName(prismicURL)
 
 interface CreateClientProps {
   req: any
 }
 
 export const prismic = ({ req }: CreateClientProps) => {
-  const client = createClient(prismicUrl, {
-    accessToken:
-      'MC5ZdXhQNEJBQUFDY0F4dnIt.77-977-977-977-9E3MDLe-_vT_vv73vv73vv71P77-977-977-977-9ZUfvv73vv73vv73vv71AVxfvv73vv71v77-977-9'
+  const client = createClient(prismicURL, {
+    accessToken: process.env.PRISMIC_TOKEN
   })
 
   prismicNext.enableAutoPreviews({ client, req })
