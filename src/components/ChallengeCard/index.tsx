34af3f00c7322ca59d31eeb8d04b0e8bf369d013
Leonardo Vargas <leu1607@hotmail.com>
2022-08-04 21:33:01 -0300

    feat: add a lib date-fns e formatando a data de encerramento

--- src/components/ChallengeCard/index.tsx
+++ src/components/ChallengeCard/index.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import ptBR from 'date-fns/locale/pt-BR'
+import { formatDistanceStrict } from 'date-fns'
 import { HiOutlineClock, HiFire, HiBadgeCheck } from 'react-icons/hi'
 
 import styles from './styles.module.scss'
@@ -10,35 +12,43 @@ interface ChallengeCardProps extends ChallengeSimple {
   challengeNumber: number
 }
 
-export const ChallengeCard = (challenge: ChallengeCardProps) => (
-  <article className={styles.challenge}>
-    {challenge.isSubmited && (
-      <span>
-        <HiBadgeCheck /> Participando
-      </span>
-    )}
+export const ChallengeCard = (challenge: ChallengeCardProps) => {
+  const daysRemaining = formatDistanceStrict(
+    new Date(),
+    new Date(challenge.deadline),
+    { unit: 'day', locale: ptBR }
+  )
 
-    <div className={styles.challenge__image}>
-      <Image src={challenge.image.url} layout="fill" objectFit="cover" />
-    </div>
+  return (
+    <article className={styles.challenge}>
+      {challenge.isSubmited && (
+        <span>
+          <HiBadgeCheck /> Participando
+        </span>
+      )}
 
-    <div className={styles.challenge__content}>
-      <small>Desafio {challenge.challengeNumber}</small>
-      <strong>{challenge.title}</strong>
+      <div className={styles.challenge__image}>
+        <Image src={challenge.image.url} layout="fill" objectFit="cover" />
+      </div>
 
-      <ul>
-        <li>
-          <HiOutlineClock />
-          {challenge.deadline} dias restantes
-        </li>
-        <li>
-          <HiFire /> 8 participantes
-        </li>
-      </ul>
+      <div className={styles.challenge__content}>
+        <small>Desafio {challenge.challengeNumber}</small>
+        <strong>{challenge.title}</strong>
 
-      <Link href={'/desafio/'.concat(challenge.id)}>
-        <a>Acessar desafio</a>
-      </Link>
-    </div>
-  </article>
-)
+        <ul>
+          <li>
+            <HiOutlineClock />
+            {daysRemaining} restantes
+          </li>
+          <li>
+            <HiFire /> 8 participantes
+          </li>
+        </ul>
+
+        <Link href={'/desafio/'.concat(challenge.id)}>
+          <a>Acessar desafio</a>
+        </Link>
+      </div>
+    </article>
+  )
+}
