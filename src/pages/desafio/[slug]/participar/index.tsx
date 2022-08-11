213b4123943ee190834b6695114e93c3d6bc4330
Leonardo Vargas <leu1607@hotmail.com>
2022-08-10 22:37:33 -0300

    feat: carregando os dados da solução no form

--- src/pages/desafio/[slug]/participar/index.tsx
+++ src/pages/desafio/[slug]/participar/index.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from 'next/router'
+import { getSession } from 'next-auth/react'
 import { HiPaperAirplane } from 'react-icons/hi'
+import { GetServerSideProps, NextPage } from 'next'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm, FormProvider } from 'react-hook-form'
 
@@ -7,14 +9,17 @@ import { SEO } from 'components/SEO'
 import { Input } from 'components/Input'
 
 import api from 'service/api'
+import { Solution } from 'types'
 import styles from './styles.module.scss'
 import { zodSolutionSchema } from 'utils/zod'
+import { SolutionModel } from 'service/mongoose'
 
-const SolutionChallengePage = () => {
+const SolutionChallengePage: NextPage<Solution | undefined> = solution => {
   const router = useRouter()
   const useFormMethods = useForm({
     mode: 'all',
-    resolver: zodResolver(zodSolutionSchema)
+    resolver: zodResolver(zodSolutionSchema),
+    defaultValues: solution
   })
 
   const onSubmit = async (data: any): Promise<void> => {
@@ -27,7 +32,7 @@ const SolutionChallengePage = () => {
       await api.post('challenge/solution', solution)
 
       // CRIAR TOAST DE SUCESSO
-      // router.push('/')
+      router.push('/')
     } catch (err) {
       console.log(err)
     }
@@ -75,6 +80,50 @@ const SolutionChallengePage = () => {
       </form>
     </FormProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  query
+}) => {
+  const session = await getSession({ req })
+
+  if (session?.user) {
+    const user_id = session.user._id
+    const challenge_id = query.slug
+
+    const solution = (
+      await SolutionModel.findOne({
+        user_id,
+        challenge_id
+      })
+    )._doc
+
+    delete solution.createdAt
+    delete solution.updatedAt
+
+    // const solution = await SolutionModel.aggregate([
+    //   {
+    //     $match: {
+    //       user_id,
+    //       challenge_id
+    //     }
+    //   },
+    //   { $project: { createdAt: 0, updatedAt: 0 } }
+    // ])
+
+    return {
+      props: {
+        ...solution,
+        user_id,
+        _id: solution._id.toString()
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
 
 export default SolutionChallengePage
