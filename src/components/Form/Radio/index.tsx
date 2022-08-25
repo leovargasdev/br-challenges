c6497cd3abc81f5165c5465938a36ef3b9a1da57
Leonardo Vargas <leu1607@hotmail.com>
2022-08-25 19:30:51 -0300

    feat: melhorias no componente de radioGroup

--- src/components/Form/Radio/index.tsx
+++ src/components/Form/Radio/index.tsx
@@ -6,14 +6,28 @@ export interface RadioOption {
   name: string
   value: string
   label: string
+  onClick: (value: string) => void
+  isChecked: boolean
 }
 
-export const Radio = ({ name, value, label }: RadioOption) => {
+export const Radio = ({
+  name,
+  value,
+  label,
+  onClick,
+  isChecked
+}: RadioOption) => {
   const { register } = useFormContext()
 
   return (
-    <div className={styles.radio}>
-      <input type="radio" id={value} value={value} {...register(name)} />
+    <div className={styles.radio} aria-checked={isChecked}>
+      <input
+        type="radio"
+        id={value}
+        value={value}
+        {...register(name)}
+        onClick={() => onClick(value)}
+      />
       <label htmlFor={value}>{label}</label>
     </div>
   )
--- src/components/Form/RadioGroup/index.tsx
+++ src/components/Form/RadioGroup/index.tsx
@@ -1,23 +1,38 @@
 import { Radio, RadioOption, ErrorMessage } from 'components/Form'
+import { useState } from 'react'
 
 import styles from './styles.module.scss'
 
 interface RadioGroupProps {
   name: string
   label: string
-  options: Omit<RadioOption, 'name'>[]
+  options: Omit<RadioOption, 'name' | 'isChecked'>[]
 }
 
-export const RadioGroup = ({ name, label, options }: RadioGroupProps) => (
-  <fieldset className={styles.container}>
-    <label className={styles.label}>{label}</label>
+export const RadioGroup = ({ name, label, options }: RadioGroupProps) => {
+  const [checked, serChecked] = useState<string>('')
 
-    <div className={styles.radios}>
-      {options.map(option => (
-        <Radio key={option.value} name={name} {...option} />
-      ))}
-    </div>
+  const handleCheckedRadio = (value: string): void => {
+    serChecked(value)
+  }
 
-    <ErrorMessage name={name} />
-  </fieldset>
-)
+  return (
+    <fieldset className={styles.container}>
+      <label className={styles.label}>{label}</label>
+
+      <div className={styles.radios}>
+        {options.map(option => (
+          <Radio
+            {...option}
+            key={option.value}
+            name={name}
+            isChecked={checked === option.value}
+            onClick={handleCheckedRadio}
+          />
+        ))}
+      </div>
+
+      <ErrorMessage name={name} />
+    </fieldset>
+  )
+}
