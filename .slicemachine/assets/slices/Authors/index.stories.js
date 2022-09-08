0765bc297e637306bdeee1ac7618d666705068a5
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-09-08 10:47:26 -0300

    chore: add docker-compose

--- .slicemachine/assets/slices/Authors/index.stories.js
+++ .slicemachine/assets/slices/Authors/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"type":"figma","link":{"link_type":"Web","url":"http://twitter.com"}}],"primary":{"name":"army","description":"aid","image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1586952518485-11b180e92764"}},"slice_type":"authors","id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"type":"github","link":{"link_type":"Web","url":"http://twitter.com"}}],"primary":{"name":"further","description":"interior","image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1515378960530-7c0da6231fb1"}},"slice_type":"authors","id":"_Default"}} />
 _Default.storyName = ''
