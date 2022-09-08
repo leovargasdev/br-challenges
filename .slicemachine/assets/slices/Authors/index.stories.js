f4a01f2d541099082e7d90085adf712e5f438b14
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-09-08 10:49:28 -0300

    chore: remove .slicemachine folder

--- .slicemachine/assets/slices/Authors/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/Authors';
-
-export default {
-  title: 'slices/Authors'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"type":"github","link":{"link_type":"Web","url":"http://twitter.com"}}],"primary":{"name":"further","description":"interior","image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1515378960530-7c0da6231fb1"}},"slice_type":"authors","id":"_Default"}} />
-_Default.storyName = ''
