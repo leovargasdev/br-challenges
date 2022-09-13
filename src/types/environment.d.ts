a84b232c45db5ce5fb530ed3fcf061991b6a4db0
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-09-13 14:31:54 -0300

    chore: adicionado tipagem para as variáveis de ambiente

---
+++ src/types/environment.d.ts
@@ -0,0 +1,14 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    GITHUB_ID: string
+    GITHUB_SECRET: string
+    GOOGLE_ID: string
+    GOOGLE_SECRET: string
+    NEXTAUTH_SECRET: string
+    NEXTAUTH_URL: string
+    MONGODB_URL: string
+    MONGODB_DB: string
+    PRISMIC_URL: string
+    PRISMIC_TOKEN: string
+  }
+}
