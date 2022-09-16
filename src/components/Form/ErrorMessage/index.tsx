6389674478f4299632b2d0de0e948606165d0383
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2022-09-16 13:57:20 -0300

    fix: remove prettier/prettier eslint plugin

--- src/components/Form/ErrorMessage/index.tsx
+++ src/components/Form/ErrorMessage/index.tsx
@@ -8,7 +8,6 @@ interface ErrorMessageProps {
 }
 
 export const ErrorMessage = ({ name }: ErrorMessageProps) => {
-  // eslint-disable-next-line prettier/prettier
   const { formState: { errors } } = useFormContext()
 
   const errorText = errors[name]?.message
