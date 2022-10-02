b19b6b64655707454b29a596ddfa08a6adf8d342
Leonardo Vargas <leu1607@hotmail.com>
2022-10-02 10:04:14 -0300

    feat: exibindo as horas restantes do desafio

--- src/utils/format/date.ts
+++ src/utils/format/date.ts
@@ -14,8 +14,20 @@ export const getFullDate = (value: string, formatString: string): string => {
 export const getDaysRemaining = (value: string): string => {
   const today = new Date()
   const dateTest = new Date(value)
-  return formatDistanceStrict(today, dateTest, {
+
+  const result = formatDistanceStrict(today, dateTest, {
     unit: 'day',
     ...dateFnsOptions
   })
+
+  if (result === '0 dias') {
+    const hours = 24 - new Date().getHours()
+    console.log(new Date().getHours())
+
+    return hours + ' horas'
+  }
+
+  console.log(result)
+
+  return result
 }
