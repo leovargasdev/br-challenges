ae8fe8a735157e1269af3c38745c3aa33e559d1a
Leonardo Vargas <leu1607@hotmail.com>
2022-12-08 07:41:35 -0300

    feat: criando o novo card, melhorias de seo e ajustes no componente de btn

--- src/components/Challenge/Card/index.tsx
+++ src/components/Challenge/Card/index.tsx
@@ -3,7 +3,6 @@ import { useSession } from 'next-auth/react'
 import { PrismicNextImage } from '@prismicio/next'
 import { HiOutlineClock, HiFire, HiCalendar } from 'react-icons/hi'
 
-import icons from './icons'
 import { Challenge } from 'types'
 import { SHORT_DATE } from 'utils/constants'
 import {
@@ -26,14 +25,7 @@ export const ChallengeCard = (challenge: Challenge) => {
   const isClosed = isChallengeClosed(status.type)
 
   return (
-    <article className={`${styles.challenge} ${styles[status.type]}`}>
-      {!['active', 'finished'].includes(status.type) && (
-        <div className={styles.challenge__status} data-type={status.type}>
-          <span>{icons[status.type]}</span>
-          <strong>{status.name}</strong>
-        </div>
-      )}
-
+    <article className={styles.challenge}>
       <div className={styles.challenge__image}>
         <PrismicNextImage
           field={challenge.image}
@@ -43,52 +35,53 @@ export const ChallengeCard = (challenge: Challenge) => {
       </div>
 
       <div className={styles.challenge__content}>
-        <small title="Autor do protótipo">{challenge.authors[0].name}</small>
-
         <h2>{challenge.name}</h2>
 
-        <ul>
-          <li>
-            <time dateTime={challenge.deadline}>
-              {isClosed ? (
-                <>
-                  <HiCalendar />
-                  {getFullDate(challenge.deadline, SHORT_DATE)}
-                </>
-              ) : (
-                <>
-                  <HiOutlineClock />
-                  {getDaysRemaining(challenge.deadline)} restantes
-                </>
-              )}
-            </time>
-          </li>
-          <li>
-            <HiFire /> {challenge.participants} participantes
-          </li>
-        </ul>
+        <div>
+          <strong aria-label="Nome do autor do protótipo do desafio">
+            {challenge.authors[0].name}
+          </strong>
 
-        <div className={styles.challenge__buttons}>
-          <Link href={`/desafio/${challenge.id}`}>
-            <a className="button outline">
-              {isClosed ? 'Detalhes' : 'Acessar desafio'}
+          <time dateTime={challenge.deadline}>
+            {isClosed ? (
+              <>
+                <HiCalendar />
+                {getFullDate(challenge.deadline, SHORT_DATE)}
+              </>
+            ) : (
+              <>
+                <HiOutlineClock />
+                {getDaysRemaining(challenge.deadline)} restantes
+              </>
+            )}
+          </time>
+        </div>
+
+        <span>
+          <HiFire /> {challenge.participants} participantes
+        </span>
+      </div>
+
+      <div className={styles.challenge__footer}>
+        <Link href={`/desafio/${challenge.id}`}>
+          <a className="button secondary">
+            {isClosed ? 'Detalhes' : 'Acessar desafio'}
+          </a>
+        </Link>
+
+        {isClosed && (
+          <Link href={`/desafio/${challenge.id}/participantes`}>
+            <a className="button" aria-disabled={status.type === 'closed'}>
+              Participantes
             </a>
           </Link>
+        )}
 
-          {isClosed && (
-            <Link href={`/desafio/${challenge.id}/participantes`}>
-              <a className="button" aria-disabled={status.type === 'closed'}>
-                Participantes
-              </a>
-            </Link>
-          )}
-
-          {status.type === 'submitted' && (
-            <Link href={`/desafio/${challenge.id}/solucao`}>
-              <a className="button">Minha solução</a>
-            </Link>
-          )}
-        </div>
+        {status.type === 'submitted' && (
+          <Link href={`/desafio/${challenge.id}/solucao`}>
+            <a className="button">Minha solução</a>
+          </Link>
+        )}
       </div>
     </article>
   )
