5f81269808471b3cdd477e05d6b7e3666c1eb4eb
Leonardo Vargas <leu1607@hotmail.com>
2023-01-09 21:41:03 -0300

    feat: alterando os links do menu Navigaiton

--- src/components/Challenge/Navigation/index.tsx
+++ src/components/Challenge/Navigation/index.tsx
@@ -1,28 +1,36 @@
+import { IconFigma, IconGitHub, IconInfo, IconYoutube } from 'components/SVG'
+
+import { useChallenge } from 'hooks'
 import styles from './styles.module.scss'
 
-export const ChallengeNavigation = () => (
-  <aside className={styles.container}>
-    <ul className={styles.content}>
-      <li>
-        <a href="#challenge-description">Descrição</a>
-      </li>
-      <li>
-        <a href="#">Nível fácil</a>
-      </li>
-      <li>
-        <a href="#">Nível médio</a>
-      </li>
-      <li>
-        <a href="#">Nível dificil</a>
-      </li>
-      <li>
-        <a href="#challenge-prototype" className={styles.active}>
-          Protótipo do desafio
-        </a>
-      </li>
-      <li>
-        <a href="#how-participation">Como participar</a>
-      </li>
-    </ul>
-  </aside>
-)
+export const ChallengeNavigation = () => {
+  const { prototype_url } = useChallenge()
+
+  return (
+    <aside className={styles.container}>
+      <ul className={styles.content}>
+        <li>
+          <a href="#" target="_blank" rel="noreferrer">
+            <IconGitHub /> Template do github
+          </a>
+        </li>
+        <li>
+          <a href={prototype_url} target="_blank" rel="noreferrer">
+            <IconFigma /> Link do protótipo
+          </a>
+        </li>
+        <li>
+          <a href="#" target="_blank" rel="noreferrer">
+            <IconYoutube /> Vídeo explicativo
+          </a>
+        </li>
+        <li>
+          <a href="/como-participar">
+            <IconInfo />
+            Como participar
+          </a>
+        </li>
+      </ul>
+    </aside>
+  )
+}
---
+++ src/components/SVG/info.tsx
@@ -0,0 +1,16 @@
+export const IconInfo = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="15"
+    height="15"
+    fill="none"
+    viewBox="0 0 15 15"
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M7.5.877a6.623 6.623 0 100 13.246A6.623 6.623 0 007.5.877zM1.827 7.5a5.673 5.673 0 1111.346 0 5.673 5.673 0 01-11.346 0zm6.423-3a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM6 6h1.5a.5.5 0 01.5.5V10h1v1H6v-1h1V7H6V6z"
+      clipRule="evenodd"
+    ></path>
+  </svg>
+)
