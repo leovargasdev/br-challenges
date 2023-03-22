c26c269deef18a29b4cadc5d6ee4256205aa3fa3
Leonardo Vargas <leu1607@hotmail.com>
2023-03-22 07:24:34 -0300

    chore: add google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,11 +38,33 @@ export default class MyDocument extends Document {
               gtag('config', 'G-TECCBMJVQH');
             `}
           </Script>
+          {/* Google Tag Manager */}
+          <Script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NSX83SJ');
+              `
+            }}
+          />
         </Head>
 
         <body>
           <Main />
           <NextScript />
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NSX83SJ"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
         </body>
       </Html>
     )
